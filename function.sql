b1b8b9775714216868c9b3a4851281375420692d
Why <3535393526@qq.com>
2024-11-12 00:31:31 +0800

    优化订单添加函数，预编译防止sql注入

--- function.sql
+++ function.sql
@@ -1,5 +1,3 @@
--- DROP PROCEDURE IF EXISTS add_order;
-
 DELIMITER $$
 -- 创建订单 传入订单ID、商品ID、店铺ID、用户ID、支付方式作为参数，并更新顾客表中的销售额  默认订单状态为Pending 默认购买数量为1
 CREATE PROCEDURE add_order(
@@ -19,13 +17,34 @@ BEGIN
 
     START TRANSACTION;
 
-    INSERT INTO goods_order (order_id, store_id, goods_id, user_id, pay_type, order_status)
-    VALUES (orderID, storeID, goodID, userID, pay, 'Pending');
+    SET @orderStatus = 'Pending';
+    SEt @orderID = orderID;
+    SET @storeID = storeID;
+    SET @goodID = goodID;
+    SET @userID = userID;
+    SET @payType = pay;
 
+    -- 创建订单的预编译语句
+    PREPARE addOrder FROM '
+    INSERT INTO goods_order (order_id, store_id, goods_id, user_id, pay_type, order_status)
+    VALUES (?, ?, ?, ?, ?, ?);';
+
+    -- 更新顾客表中的销售预编译语句
+    PREPARE updateCustomerSales FROM '
     UPDATE goods
     SET monthly_sales_volume = monthly_sales_volume + (price * 1)
-    WHERE store_id = storeID
-    AND goods_id = goodID;
+    WHERE store_id = ?
+    AND goods_id = ?;';
+
+    -- 插入订单数据
+    EXECUTE addOrder USING @orderID, @storeID, @goodID, @userID, @payType, @orderStatus;
+
+    -- 更新顾客表中的销售额
+    EXECUTE updateCustomerSales USING @storeID, @goodID;
+
+    -- 释放预编译语句
+    DROP PREPARE addOrder;
+    DROP PREPARE updateCustomerSales;
 
     COMMIT;
 
@@ -33,3 +52,9 @@ BEGIN
 
 END$$
 DELIMITER ;
+
+
+/*
+-- 删除函数
+# DROP PROCEDURE add_order;
+ */
